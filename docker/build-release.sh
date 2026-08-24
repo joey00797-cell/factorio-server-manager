60611aba41595c9c41e8746344abdc2ef2cefcdf
root <root@fsm-test2.loc>
2026-08-24 18:47:54 +0000

    fix: skip Windows zip copy in build-release.sh when BUILD_WINDOWS not set

--- docker/build-release.sh
+++ docker/build-release.sh
@@ -7,4 +7,7 @@ echo "Creating build..."
 make gen_release
 echo "Copying build artifacts from ${PWD}"
 mkdir -p /build
-cp -v build/factorio-server-manager-linux.zip build/factorio-server-manager-windows.zip /build/
+cp -v build/factorio-server-manager-linux.zip /build/
+if [ "${BUILD_WINDOWS}" = "1" ] && [ -f build/factorio-server-manager-windows.zip ]; then
+    cp -v build/factorio-server-manager-windows.zip /build/
+fi
